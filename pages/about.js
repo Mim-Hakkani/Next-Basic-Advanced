af272742858d4e4b6f9cdcf72240becb94f0fc4c
32-Mimhakkani <mim.cse32@gmail.com>
2022-10-03 17:21:36 +0600

    next js routing

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const about = () => {
+    return (
+        <div>
+            Name : Ehsan Marketing 
+            Location : Bogura,Bangladesh.
+            Post : software Engr.
+        </div>
+    );
+};
+
+export default about;
