0d34f1eae04eba031ec6e98cb96b321f6ff9b87f
32-Mimhakkani <mim.cse32@gmail.com>
2022-10-03 16:21:32 +0600

    make a component folder ,images folder ,

---
+++ Components/Hakkani.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Hakkani = ({name}) => {
+    // console.log('Name is :: ',name);
+    return (
+        <div>
+            <h1>
+                Wellcome To Ehsan Markeing employee is {name}
+            </h1>
+        </div>
+    );
+};
+
+export default Hakkani;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Hakkani from '../Components/Hakkani'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,6 +12,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/* show the personal details  */}
+
+      <Hakkani name="Golam Hakakni"/>
   
     </div>
   )
